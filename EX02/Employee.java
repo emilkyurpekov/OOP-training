7bb2f4158d58049c0fe2b4c32d9559afbdc16bfe
Emil Kyurpekov <64215369+emilkyurpekov@users.noreply.github.com>
2023-05-30 17:34:46 +0300

    Add files via upload
    This program defines a class Employee that holds the following information: name, salary, position, department, email, and age. The name, salary, position, and department are mandatory, while the rest are optional.  
    My task was to write a program that takes N lines of information about employees from the console and calculates the department with the highest average salary, and prints for each employee in that department his name, salary, email, and age - sorted by salary in descending order. If an employee doesn't have an email – in place of that field, it should print "n/a" instead, if he doesn't have an age – print "-1" instead.

---
+++ EX02/Employee.java
@@ -0,0 +1,79 @@
+package ClassesAndObjects.EX02;
+
+public class Employee {
+    private  String name;
+    private  double salary;
+    private  String position;
+    private  String department;
+    private String email;
+    int age;
+
+    public Employee(String name, double salary, String position, String department, String email, int age) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.email = email;
+        this.age = age;
+    }//all the info
+
+    public Employee(String name, double salary, String position, String department) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.setEmail("n/a");
+        this.setAge(-1);
+    }//all the mandatory info
+
+    public Employee(String name, double salary, String position, String department, String email) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.email = email;
+        this.setAge(-1);
+    }//no age
+
+    public Employee(String name, double salary, String position, String department, int age) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.age = age;
+        this.setEmail("n/a");
+    }// no email
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.2f %s %d", name, salary, email, age);
+    }
+}
